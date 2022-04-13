d4a6eb5f7a7e735fb679857e3b63533cb278d25e
atKarl <karl-emmanuel@outlook.com>
2022-04-13 15:51:20 +0200

    Complete Implementation with Random champions

--- script.js
+++ script.js
@@ -1,2 +1,364 @@
-const targetWords =[];
-const dictionary = [];
+let names = [
+  "aatrox",
+  "ahri",
+  "akali",
+  "amumu",
+  "akshan",
+  "alistar",
+  "anivia",
+  "annie",
+  "aphelios",
+  "ashe",
+  "aurelionsol",
+  "azir",
+  "bard",
+  "blitzcrank",
+  "brand",
+  "braum",
+  "caitlyn",
+  "camille",
+  "cassiopeia",
+  "chogath",
+  "corki",
+  "darius",
+  "diana",
+  "ekko",
+  "elise",
+  "evelynn",
+  "ezreal",
+  "fiddlesticks",
+  "fiora",
+  "fizz",
+  "galio",
+  "gangplank",
+  "garen",
+  "gnar",
+  "gragas",
+  "graves",
+  "gwen",
+  "hecarim",
+  "heimerdinger",
+  "illaoi",
+  "irelia",
+  "ivern",
+  "janna",
+  "jarvaniv",
+  "jax",
+  "jayce",
+  "jhin",
+  "jinx",
+  "kaisa",
+  "kalista",
+  "karma",
+  "karthus",
+  "kassadin",
+  "katarina",
+  "kayle",
+  "kayn",
+  "kennen",
+  "khazix",
+  "kindred",
+  "kled",
+  "kogmaw",
+  "leblanc",
+  "leesin",
+  "leona",
+  "lillia",
+  "lissandra",
+  "lucian",
+  "lulu",
+  "lux",
+  "masteryi",
+  "malphite",
+  "malzahar",
+  "maokai",
+  "missfortune",
+  "mordekaiser",
+  "morgana",
+  "nami",
+  "nasus",
+  "nautilus",
+  "neeko",
+  "nidalee",
+  "nocturne",
+  "nunuandwillump",
+  "olaf",
+  "orianna",
+  "ornn",
+  "pantheon",
+  "poppy",
+  "pyke",
+  "qiyana",
+  "quinn",
+  "rakan",
+  "rammus",
+  "reksai",
+  "rell",
+  "renata",
+  "renekton",
+  "rengar",
+  "riven",
+  "rumble",
+  "ryze",
+  "samira",
+  "sejuani",
+  "senna",
+  "seraphine",
+  "sett",
+  "shaco",
+  "shen",
+  "shyvana",
+  "singed",
+  "sion",
+  "sivir",
+  "skarner",
+  "sona",
+  "soraka",
+  "swain",
+  "sylas",
+  "syndra",
+  "tahmkench",
+  "taliyah",
+  "talon",
+  "taric",
+  "teemo",
+  "thresh",
+  "tristana",
+  "trundle",
+  "tryndamere",
+  "twistedfate",
+  "udyr",
+  "urgot",
+  "varus",
+  "vayne",
+  "veigar",
+  "velkoz",
+  "vex",
+  "vi",
+  "viego",
+  "viktor",
+  "vladimir",
+  "volibear",
+  "warwick",
+  "wukong",
+  "xayah",
+  "xerath",
+  "xinzhao",
+  "yasuo",
+  "yone",
+  "yorick",
+  "yuumi",
+  "zac",
+  "zed",
+  "zeri",
+  "ziggs",
+  "zilean",
+  "zoe",
+  "zyra",
+];
+
+let six = [];
+let five = [];
+let seven = [];
+let four = [];
+
+
+for (let i = 0; i < names.length; i++) {
+  switch (names[i].length) {
+    case 6:
+      six.push(names[i]);
+      break;
+    case 5:
+      five.push(names[i]);
+      break;
+    case 7:
+      seven.push(names[i]);
+      break;
+    case 4:
+      four.push(names[i]);
+      break;
+  }
+}
+
+const targetWords = [...five];
+const dictionary = [...five];
+const WORD_LENGTH = 5;
+// const offsetFromDate = new Date(2022, 0, 1);
+// const msOffset = Date.now() - offsetFromDate;
+// const dayOffset = msOffset /1000 / 60 / 60 / 24;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
+const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)];
+const keyboard = document.querySelector("[data-keyboard]");
+let guessGrid = document.querySelector("[data-guess-grid]");
+let alertContainer = document.querySelector("[data-alert-container]");
+
+let startInteraction = () => {
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+};
+
+let stopInteraction = () => {
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
+};
+
+let handleMouseClick = (e) => {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
+  if (e.target.matches("[data-enter]")) {
+    submitGuess();
+    return;
+  }
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
+};
+
+let handleKeyPress = (e) => {
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey();
+    return;
+  }
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key);
+    return;
+  }
+};
+
+let pressKey = (key) => {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
+};
+let deleteKey = () => {
+  let activeTiles = getActiveTiles();
+  let lastTile = activeTiles[activeTiles.length - 1];
+  if (lastTile === null) return;
+  lastTile.textContent = "";
+  delete lastTile.dataset.state;
+  delete lastTile.dataset.letter;
+};
+
+let submitGuess = () => {
+  const activeTiles = [...getActiveTiles()];
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert(`Please enter a ${WORD_LENGTH} letter word.`);
+    shakeTiles(activeTiles);
+    return;
+  }
+  const guess = activeTiles.reduce((word, tile) => {
+    return word + tile.dataset.letter;
+  }, "");
+  if (!dictionary.includes(guess)) {
+    showAlert("Enter a champion name");
+    shakeTiles(activeTiles);
+    return;
+  }
+  stopInteraction();
+  activeTiles.forEach((...params) => flipTile(...params, guess));
+};
+
+let flipTile = (tile, index, array, guess) => {
+  const letter = tile.dataset.letter;
+  const key = keyboard.querySelector(`[data-key="${letter}"i]`);
+  setTimeout(() => {
+    tile.classList.add("flip");
+  }, (index * FLIP_ANIMATION_DURATION) / 2);
+
+  tile.addEventListener(
+    "transitionend",
+    () => {
+      tile.classList.remove("flip");
+      if (targetWord[index] === letter) {
+        tile.dataset.state = "correct";
+        key.classList.add("correct");
+      } else if (targetWord.includes(letter)) {
+        tile.dataset.state = "wrong-location";
+        key.classList.add("wrong-location");
+      } else {
+        tile.dataset.state = "wrong";
+        key.classList.add("wrong");
+      }
+      if (index === array.length - 1) {
+        tile.addEventListener(
+          "transitionend",
+          () => {
+            startInteraction();
+            checkWinLose(guess, array);
+          },
+          { once: true }
+        );
+      }
+    },
+    { once: true }
+  );
+};
+let getActiveTiles = () => guessGrid.querySelectorAll("[data-state='active']");
+
+let showAlert = (message, duration = 1000) => {
+  const alert = document.createElement("div");
+  alert.textContent = message;
+  alert.classList.add("alert");
+  alertContainer.prepend(alert);
+  if (duration === null) return;
+  setTimeout(() => {
+    alert.classList.add("hide");
+    alert.addEventListener("transitionend", () => alert.remove());
+  }, duration);
+};
+
+let shakeTiles = (tiles) => {
+  tiles.forEach((tile) => {
+    tile.classList.add("shake");
+    tile.addEventListener(
+      "animationend",
+      () => {
+        tile.classList.remove("shake");
+      },
+      { once: true }
+    );
+  });
+};
+let checkWinLose = (guess, tiles) => {
+  if (guess === targetWord) {
+    showAlert("You Win", 5000);
+    danceTiles(tiles);
+    stopInteraction();
+    return;
+  }
+  const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])")
+  if (remainingTiles.length===0){
+      showAlert(targetWord.toLocaleUpperCase(), null);
+      stopInteraction();
+  }
+};
+
+let danceTiles = (tiles) => {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("dance");
+      tile.addEventListener(
+        "animationend",
+        () => {
+          tile.classList.remove("dance");
+        },
+        { once: true }
+      );
+    }, index * DANCE_ANIMATION_DURATION / 5);
+  });
+};
+
+startInteraction();
+
